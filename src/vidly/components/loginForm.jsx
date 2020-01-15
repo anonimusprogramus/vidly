fa6d31b6d11d0edbca150456c3da0a2d25e06efb
Anonimus Programus <anonimus.programus@gmail.com>
2020-01-15 11:23:05 +0700

    127 done, ready to extract form validation

--- src/vidly/components/loginForm.jsx
+++ src/vidly/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Joi from 'joi-browser'
 import Input from './common/input'
 
 class LoginForm extends Component {
@@ -7,22 +8,32 @@ class LoginForm extends Component {
     errors: {}
   }
 
+  schema = {
+    username: Joi.string()
+      .required()
+      .label('Username'),
+
+    password: Joi.string()
+      .required()
+      .label('Password')
+  }
+
   validate = () => {
+    console.log('gonna validate')
+    const options = { abortEarly: false }
+    const { error } = Joi.validate(this.state.account, this.schema, options)
+    if (!error) return null
+
     const errors = {}
-
-    const { account } = this.state
-    if (account.username.trim() === '') errors.username = 'Username is required'
-    if (account.password.trim() === '') errors.password = 'Password is required'
-
-    return Object.keys(errors).length === 0 ? null : errors
+    for (let item of error.details) errors[item.path[0]] = item.message
+    return errors
   }
 
   validateProperty = ({ name, value }) => {
-    if (name === 'username')
-      if (value.trim() === '') return 'Username is required'
-
-    if (name === 'password')
-      if (value.trim() === '') return 'Password is required'
+    const obj = { [name]: value }
+    const schema = { [name]: this.schema[name] }
+    const { error } = Joi.validate(obj, schema)
+    return error ? error.details[0].message : null
   }
 
   handleSubmit = e => {
@@ -68,7 +79,9 @@ class LoginForm extends Component {
             onChange={this.handleChange}
             error={errors.password}
           />
-          <button className='btn btn-primary'>Login</button>
+          <button disabled={this.validate()} className='btn btn-primary'>
+            Login
+          </button>
         </form>
       </div>
     )
