b42f26183ea04ec518a0175604a0cd926cf77dcc
Anonimus Programus <anonimus.programus@gmail.com>
2020-01-19 16:02:11 +0700

    190 done, ready for production

--- src/vidly/components/moviesTable.jsx
+++ src/vidly/components/moviesTable.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import auth from '../services/authService'
+
 import Like from './common/like'
 import Table from './common/table'
 import { Link } from 'react-router-dom'
@@ -18,19 +20,27 @@ class MoviesTable extends Component {
       content: movie => (
         <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
       )
-    },
-    {
-      key: 'delete',
-      content: movie => (
-        <button
-          onClick={() => this.props.onDelete(movie)}
-          className='btn btn-danger btn-sm'
-        >
-          Delete
-        </button>
-      )
     }
   ]
+
+  deleteColumn = {
+    key: 'delete',
+    content: movie => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className='btn btn-danger btn-sm'
+      >
+        Delete
+      </button>
+    )
+  }
+
+  constructor() {
+    super()
+    const user = auth.getCUrrentUser()
+    if (user && user.isAdmin) this.columns.push(this.deleteColumn)
+  }
+
   render() {
     const { movies, onSort, sortColumn } = this.props
 
